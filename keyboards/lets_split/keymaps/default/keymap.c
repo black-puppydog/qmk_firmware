f75d77e8c1ec8fdd328db35db2c6ff6662022b5a
Daan Wynen <black.puppydog@gmx.de>
2020-08-25 16:29:23 +0200

    reformatting

--- keyboards/lets_split/keymaps/default/keymap.c
+++ keyboards/lets_split/keymaps/default/keymap.c
@@ -35,10 +35,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12( \
-  KC_TAB,          KC_Q,   KC_W,     KC_E,     KC_R,   KC_T,    KC_Y,    KC_U,   KC_I,     KC_O,    KC_P,     KC_DEL, \
-  KC_BSPC,         KC_A,   KC_S,     KC_D,     KC_F,   KC_G,    KC_H,    KC_J,   KC_K,     KC_L,    KC_SCLN,  KC_QUOT, \
-  KC_LSFT,         KC_Z,   KC_X,     KC_C,     KC_V,   KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,  KC_SFTENT, \
-  LCTL_T(KC_ESC),  ADJUST,  KC_LGUI,  KC_LALT,  LOWER,  KC_SPC,  KC_SPC,  RAISE,  KC_RALT,  KC_APP,  KC_UP,    KC_RGHT \
+  KC_TAB,          KC_Q,    KC_W,     KC_E,     KC_R,   KC_T,    KC_Y,    KC_U,   KC_I,     KC_O,    KC_P,     KC_DEL,     \
+  KC_BSPC,         KC_A,    KC_S,     KC_D,     KC_F,   KC_G,    KC_H,    KC_J,   KC_K,     KC_L,    KC_SCLN,  KC_QUOT,    \
+  KC_LSFT,         KC_Z,    KC_X,     KC_C,     KC_V,   KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,  KC_SFTENT,  \
+  LCTL_T(KC_ESC),  ADJUST,  KC_LGUI,  KC_LALT,  LOWER,  KC_SPC,  KC_SPC,  RAISE,  KC_RALT,  KC_APP,  KC_UP,    KC_RGHT     \
 ),
 
 
@@ -55,10 +55,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_ortho_4x12( \
-  _______,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,     KC_Y,    KC_SCLN,  _______, \
-  _______,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,     KC_I,    KC_O,     KC_QUOT, \
-  _______,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  _______, \
-  _______,  _______,  _______,  _______,  _______,  KC_SPC,  KC_SPC,  _______,  _______,   _______,  _______,   _______ \
+  _______,  KC_Q,     KC_W,     KC_F,     KC_P,     KC_G,    KC_J,    KC_L,     KC_U,     KC_Y,     KC_SCLN,  _______,  \
+  _______,  KC_A,     KC_R,     KC_S,     KC_T,     KC_D,    KC_H,    KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,  \
+  _______,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_K,    KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  _______,  \
+  _______,  _______,  _______,  _______,  _______,  KC_SPC,  KC_SPC,  _______,  _______,  _______,  _______,  _______   \
 ),
 /* Dvorak
  * ,-----------------------------------------|-----------------------------------------.
@@ -72,10 +72,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_DVORAK] = LAYOUT_ortho_4x12( \
-  _______,  KC_QUOT,  KC_COMM,  KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    _______, \
-  _______,  KC_A,     KC_O,     KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, \
-  _______,  KC_SCLN,  KC_Q,     KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    _______, \
-  _______,  _______,   _______,   _______,  _______,  KC_SPC,  KC_SPC,  _______,  _______,  _______,  _______,  _______ \
+  _______,  KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,    KC_F,    KC_G,     KC_C,     KC_R,     KC_L,     _______,  \
+  _______,  KC_A,     KC_O,     KC_E,     KC_U,     KC_I,    KC_D,    KC_H,     KC_T,     KC_N,     KC_S,     KC_SLSH,  \
+  _______,  KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,    KC_B,    KC_M,     KC_W,     KC_V,     KC_Z,     _______,  \
+  _______,  _______,  _______,  _______,  _______,  KC_SPC,  KC_SPC,  _______,  _______,  _______,  _______,  _______   \
 ),
 
 /* Lower
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 )
 
 
